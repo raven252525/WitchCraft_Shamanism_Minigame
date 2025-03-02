5bee913c608c37baa0ecbba9e42a0e913ab536dd
raven252525 <maximyurkovsky@gmail.com>
2025-03-02 12:27:11 -0800

    added tile compatability and drew temporary map

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 import { k } from "./kaboomCtx";
+
+//function to load img as a sprite, and can dictate frames to use, veets allows direct acccess via .
+//3rd param is an obj that specifics how to slice img into frames
+k.loadSprite("spritesheet", "./spritesheet.png", {
+    sliceX: 39,
+    sliceY: 31,
+    anims: {
+        //hookup specific frames to specific functions
+        "idle-down": 936,
+        "walk-down" : { from: 936, to: 939, loop:true, speed: 8},
+        "idle-side": 975,
+        "walk-side" : { from: 975, to: 978, loop:true, speed: 8},
+        "idle-down": 1014,
+        "walk-down" : { from: 1014, to: 1017, loop:true, speed: 8},
+    },
+});
