dd8028e37d0dbc44fddb834bf9808e1eb9f52747
raven252525 <maximyurkovsky@gmail.com>
2025-03-04 22:10:00 -0800

    added functional expression to reset player sprite to idle animation depending on direction of last movement

--- src/main.js
+++ src/main.js
@@ -151,6 +151,20 @@ k.scene("main", async () => {
             return;
         }
     });
+
+    // depending on direction of animation at mouse release, end animation and go to idle frame
+    k.onMouseRelease(() => {
+        if(player.direction === "down") {
+            player.play("idle-down");
+            return;
+        }
+        if (player.direction === "up") {
+            player.play("idle-up");
+            return;
+        }
+        
+        player.play("idle-side");
+    });
 });
 
 k.go("main");
