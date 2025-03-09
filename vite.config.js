cb17799797789d35cc3fd55ce4408750766d1716
raven252525 <maximyurkovsky@gmail.com>
2025-03-08 23:05:31 -0800

    added / to end of base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/WitchCraft_Shamanism_Minigame",
+    base: "/WitchCraft_Shamanism_Minigame/",
     build: {
         minify: "terser",
     },
